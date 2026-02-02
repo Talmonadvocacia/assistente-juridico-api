c23bd05f3523731cf5635876c141512fd1d6a023
Talmonadvocacia <programastalmon@gmail.com>
2026-02-02 15:58:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route("/health")
 def health():
     return {"status": "ok"}
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Mensagem recebida:", data)
+    return {
+        "status": "recebido",
+        "mensagem": data
+    }
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
